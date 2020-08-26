45cdc77ef16978335a2a5dc68981f48461c4b003
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-08-26 14:59:55 +0400

    Create Validating phone numbers.py

---
+++ Validating phone numbers.py
@@ -0,0 +1,16 @@
+n=int(input())
+for i in range(n):
+    m=input()
+    if m.isdigit():
+        m=int(m)
+        k=0
+        while m>=1:
+            m=m/10
+            k+=1
+        m=int(m*10)
+        if k==10 and (m==7 or m==8 or m==9):
+            print("YES") 
+        else:
+            print("NO")
+    else:
+        print("NO")
