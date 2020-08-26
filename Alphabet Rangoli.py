f10396617266c04120eaaa79c8083bf04e1f1310
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-08-26 12:45:25 +0400

    Update Alphabet Rangoli.py

--- Alphabet Rangoli.py
+++ Alphabet Rangoli.py
@@ -1,6 +1,10 @@
+import string
+
 def print_rangoli(n):
-    list1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    list1=string.ascii_lowercase
     list2=list1[0:n]
+
+    #outputun [1;n-1]ci setirleri
     for i in range(n-1):
         print ("-"*((2*n-2)-2*i),end="")
         for x in range(1,i+2):
@@ -13,6 +17,7 @@ def print_rangoli(n):
         else:
             print("-"*(((2*n-2)-2*i)-1))
 
+    #outputun n-ci setiri 
     for i in range(1,n):
         print (f"{list2[-i]}-",end="")
     print("a",end="")
@@ -20,6 +25,7 @@ def print_rangoli(n):
         print (f"-{list2[i]}",end="")
     print()
 
+    #outputun [n+1;2n]ci setirleri
     for i in range(n-2,-1,-1):
         print ("-"*((2*n-2)-2*i),end="")
         for x in range(1,i+2):
