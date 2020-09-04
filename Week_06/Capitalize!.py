9526fb2d78b0c703a136e19aa7d9c8aa05c8efec
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-09-04 19:33:46 +0400

    Create Capitalize!.py

---
+++ Week_06/Capitalize!.py
@@ -0,0 +1,16 @@
+def solve(s):
+    list1 = list(s)
+    t = len(list1)
+    list1[0] = list1[0].upper()
+    for i in range(t):
+        if list1[i] == " ":
+            list1[i+1] = list1[i+1].upper()
+        else:
+            pass
+    s = ''.join(list1)
+    return s
+
+
+s = input()
+result = solve(s)
+print(result)
