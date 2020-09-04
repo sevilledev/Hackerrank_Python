df988995e9f8e6e0051224ba0a8662373ec27844
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-09-04 17:33:17 +0400

    Create String Validators.py

---
+++ Week_05/String Validators.py
@@ -0,0 +1,36 @@
+s = input()
+for i in s:
+    if i.isalnum()==True:
+        print(True)
+        break
+else:
+    print(False)
+    
+for i in s:
+    if i.isalpha()==True:
+        print(True)
+        break
+else:
+    print(False)
+    
+for i in s:
+    if i.isdigit()==True:
+        print(True)
+        break
+else:
+    print(False)
+    
+for i in s:
+    if i.islower()==True:
+        print(True)
+        break
+else:
+    print(False)
+    
+for i in s:
+    if i.isupper()==True:
+        print(True)
+        break
+else:
+    print(False)
+
