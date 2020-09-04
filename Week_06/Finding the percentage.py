9ebf8c2a0ff794d92cc18b778cd68e35aa7dc092
Sevil Rasulova <68187750+Sevil2005@users.noreply.github.com>
2020-09-04 20:13:17 +0400

    Create Finding the percentage.py

---
+++ Week_06/Finding the percentage.py
@@ -0,0 +1,12 @@
+n = int(input())
+student_marks = {}
+
+for i in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+    
+query_name = input()
+student_marks = student_marks[query_name]
+t = student_marks[0]+student_marks[1]+student_marks[2]
+print('{0:.{1}f}'.format(t/3, 2))
